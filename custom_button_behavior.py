b1f041d33dd385a8155a12af9b546f6632882c6b
npool9 <npool@elon.edu>
2020-05-06 11:31:31 -0400

    Updated button behavior for certain conditions

--- custom_button_behavior.py
+++ custom_button_behavior.py
@@ -8,6 +8,7 @@ class CustomButtonBehavior(ButtonBehavior):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.prev_state = None
+        self.seconds = 0
 
     def _do_press(self, *args):
         """
@@ -20,7 +21,9 @@ class CustomButtonBehavior(ButtonBehavior):
         """
         Override
         """
-        if self.prev_state == 'normal1':
+        if self.prev_state == 'normal1' and self.seconds > 0:
             self.state = 'normal2'
         elif self.prev_state == 'normal2':
             self.state = 'normal1'
+        else:
+            self.state = self.prev_state
--- main_page.py
+++ main_page.py
@@ -8,6 +8,8 @@ class MainPage(RelativeLayout):
     """
     The main page of the application and its various functions
     """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def calculate_time(self):
         """
@@ -24,6 +26,7 @@ class MainPage(RelativeLayout):
                 print("Please input a time greater than 0.")
             else:
                 self.start_countdown(seconds)
+                return seconds
             return seconds
         except ValueError:
             print("Please input numbers in the text boxes.")
@@ -34,14 +37,16 @@ class MainPage(RelativeLayout):
         :param seconds: number of seconds chosen by the user until phone turns off
         """
         # TODO: start new thread for stop watch
-        print("Turn off phone in", str(seconds), "seconds.")
+        print("Turn off phone in", "\033[1m" + str(seconds) + "\033[0m", "seconds.")
 
     def stop_timer(self):
         """
         Interrupt the stopwatch thread and stop the timer.
+        :return: 0 seconds
         """
         # TODO: stop the timer
         print("Stop the timer.")
         app = App.get_running_app()  # get the app's root
         app.root.ids.hours.text = '0'
         app.root.ids.minutes.text = '0'
+        return 0
